52c7ba1bd6e9596a186dbb77d26bc4862c818283
ryosuke84 <a.bianco84@gmail.com>
2018-12-28 16:15:44 +0100

    Conveterd class to factory function

--- src/creature.js
+++ src/creature.js
@@ -1,124 +1,86 @@
-class Creature {
-  constructor({ p5Instance, initX, initY, maxVelocity, maxSteeringVelocity, dampening }) {
-    this.p5 = p5Instance;
-    this.location = this.p5.createVector(initX, initY);
-    this.velocity = this.p5.createVector();
-    this.acceleration = this.p5.createVector();
-    this.maxVelocity = maxVelocity;
-    this.maxSteeringVelocity = maxSteeringVelocity;
-    this.dampening = dampening;
-    this.vHeight = 20;
-    this.vWidth = 10;
-    this.v1 = null;
-    this.v2 = null;
-    this.v3 = null;
-  }
+export default ({ p5Instance, initX, initY, maxVelocity, maxSteeringVelocity, dampening }) => {
+  const p5 = p5Instance;
 
-  _draw() {
-    const p5 = this.p5;
+  const _draw = self => {
+    // const p5 = this.p5;
     p5.stroke(0);
     p5.fill(175);
 
-    this.v1 = p5.createVector(0, 0);
-    this.v2 = p5.createVector(-this.vWidth / 2, this.vHeight);
-    this.v3 = p5.createVector(this.vWidth / 2, this.vHeight);
+    self.v1 = p5.createVector(0, 0);
+    self.v2 = p5.createVector(-self.vWidth / 2, self.vHeight);
+    self.v3 = p5.createVector(self.vWidth / 2, self.vHeight);
 
-    p5.triangle(this.v1.x, this.v1.y, this.v2.x, this.v2.y, this.v3.x, this.v3.y);
-  }
+    p5.triangle(self.v1.x, self.v1.y, self.v2.x, self.v2.y, self.v3.x, self.v3.y);
+  };
 
-  _displayVehicle() {
-    const p5 = this.p5;
+  const _displayVehicle = self => {
+    // const p5 = this.p5;
     p5.push();
-    p5.translate(this.location.x, this.location.y);
-    p5.rotate(this.velocity.heading() + p5.PI / 2);
-    this._draw();
+    p5.translate(self.location.x, self.location.y);
+    p5.rotate(self.velocity.heading() + p5.PI / 2);
+    _draw(self);
     p5.pop();
-  }
+  };
 
-  _checkBoundaries() {
-    const p5 = this.p5;
-    const x = this.location.x;
-    const y = this.location.y;
+  const _checkBoundaries = self => {
+    // const p5 = this.p5;
+    const x = self.location.x;
+    const y = self.location.y;
 
     if (x < 0) {
-      this.location.x = p5.width;
+      self.location.x = p5.width;
     }
 
     if (x > p5.width) {
-      this.location.x = 0;
+      self.location.x = 0;
     }
 
     if (y < 0) {
-      this.location.y = p5.height;
+      self.location.y = p5.height;
     }
 
     if (y > p5.height) {
-      this.location.y = 0;
+      self.location.y = 0;
     }
-  }
+  };
 
-  render() {
-    this._displayVehicle();
-    // this._displayFov();
-  }
+  return {
+    location: p5.createVector(initX, initY),
+    velocity: p5.createVector(),
+    acceleration: p5.createVector(),
+    maxVelocity,
+    maxSteeringVelocity,
+    dampening,
+    vHeight: 20,
+    vWidth: 10,
+    v1: null,
+    v2: null,
+    v3: null,
 
-  applyForce(force) {
-    force.limit(this.maxSteeringVelocity);
-    this.acceleration.add(force);
-  }
+    render() {
+      _displayVehicle(this);
+      // this._displayFov();
+    },
 
-  update() {
-    // const p5 = this.p5;
-    this.velocity.add(this.acceleration);
-    this.location.add(this.velocity);
-    this.velocity.limit(this.maxVelocity);
-    this.velocity.mult(this.dampening);
+    applyForce(force) {
+      force.limit(this.maxSteeringVelocity);
+      this.acceleration.add(force);
+    },
 
-    this.acceleration.mult(0);
-  }
+    update() {
+      // const p5 = this.p5;
+      this.velocity.add(this.acceleration);
+      this.location.add(this.velocity);
+      this.velocity.limit(this.maxVelocity);
+      this.velocity.mult(this.dampening);
 
-  // getFovPoints() {
-  //   const start = this.velocity.heading() - this.fovAngle;
-  //   const end = this.velocity.heading() + this.fovAngle;
-  //   const T1 = start >= -Math.PI ? start : start + Math.PI * 2;
-  //   const T2 = end < Math.PI ? end : end - Math.PI * 2;
-  //   // const T1 = start;
-  //   // const T2 = end;
-  //   // console.log('T1: ' + T1 + ', T2: ' + T2)
+      this.acceleration.mult(0);
+    },
 
-  //   //Check if start and end angles are into II and III quadrant
-  //   const reversedAngles = T1 > T2;
-
-  //   // let count = 0;
-  //   const points = [];
-  //   for (let x = this.location.x - this.fovRadius; x < this.location.x + this.fovRadius; x++) {
-  //     for (let y = this.location.y - this.fovRadius; y < this.location.y + this.fovRadius; y++) {
-  //       const r = Math.sqrt(Math.pow(x - this.location.x, 2) + Math.pow(y - this.location.y, 2));
-  //       let theta = Math.atan2(y - this.location.y, x - this.location.x);
-
-  //       if (
-  //         r < this.fovRadius &&
-  //         //  The condition to check whenstart and end angles are into II and III quadrant --------
-  //         ((reversedAngles && ((theta >= -Math.PI && theta < T2) || (theta > T1 && theta <= Math.PI))) ||
-  //           //  The condition to check when start and end angles are not into II and III quadrant --------
-  //           (!reversedAngles && theta > T1 && theta < T2))
-  //       ) {
-  //         // if (r < radius && theta > T1 && theta < T2) {
-  //         points.push({ x: Math.floor(x), y: Math.floor(y) });
-  //         // p5.point(Math.floor(x), Math.floor(y));
-  //         // count++;
-  //         // positions.push({ x: Math.floor(x), y: Math.floor(y) });
-  //       }
-  //     }
-  //   }
-  //   return points;
-  // }
-
-  run() {
-    this.update();
-    this._checkBoundaries();
-    this.render();
-  }
-}
-
-export default Creature;
+    run() {
+      this.update();
+      _checkBoundaries(this);
+      this.render();
+    }
+  };
+};
--- src/index.js
+++ src/index.js
@@ -1,67 +1,15 @@
-import "./styles.css";
-import p5 from "p5";
-import "p5/lib/addons/p5.dom";
-import compose from "lodash.flowright";
+// export const createBoid({ p5Instance, initX, initY, maxVelocity, maxSteeringVelocity, dampening }) => ({
+//   p5 : p5Instance,
+//   location = p5.createVector(initX, initY),
+//   velocity = p5.createVector(),
+//   acceleration = p5.createVector(),
+//   maxVelocity,
+//   maxSteeringVelocity,
+//   dampening,
+//   vHeight : 20,
+//   vWidth : 10,
+//   v1 : null,
+//   v2 : null,
+//   v3 = null
 
-import Boid from "@ryosuke84/boid";
-import Creature from "./creature.js";
-import ControllableBoid from "./controllableBoid.js";
-
-import { GoRandomMixin, createFovMixin, MouseSeekingMixin } from "./behaviors.js";
-
-const sketch = p5 => {
-  let canvas;
-  let creature;
-
-  p5.setup = () => {
-    canvas = p5.createCanvas(600, 400);
-    canvas.style("border", "solid 1px");
-    p5.background(255);
-
-    const RandomFovBoid = compose([GoRandomMixin, createFovMixin({ fovRadius: 60, fovAngle: Math.PI / 4 }), MouseSeekingMixin])(Boid);
-
-    creature = new RandomFovBoid({
-      p5Instance: p5,
-      initX: p5.width / 2,
-      initY: p5.height / 2,
-      maxVelocity: 3,
-      maxSteeringVelocity: 0.3,
-      dampening: 0.93
-    });
-  };
-
-  p5.draw = () => {
-    p5.background(255);
-
-    creature.run();
-    creature.renderFov();
-
-    if (creature.updateFovPoints) {
-      const fovPoints = creature.updateFovPoints();
-      for (let i = 0; i < fovPoints.length; i++) {
-        if (i % 200 === 0) {
-          p5.push();
-          p5.stroke("red");
-          p5.strokeWeight(5);
-          p5.point(fovPoints[i].x, fovPoints[i].y);
-          p5.pop();
-        } else {
-          // p5.point(fovPoints[i].x, fovPoints[i].y);
-        }
-      }
-    }
-
-    if (p5.mouseIsPressed && creature.seekMouse) {
-      creature.seekMouse(p5.mouseX, p5.mouseY);
-    } else {
-      creature.goRandom();
-    }
-  };
-};
-
-const element = document.getElementById("app");
-while (element && element.firstChild) {
-  element.removeChild(element.firstChild);
-}
-
-new p5(sketch, "app");
+// })
